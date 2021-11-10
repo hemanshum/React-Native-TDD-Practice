26cda3c716969e8e3f878fdcf49e0e6e5a25ecc8
Hemanshu <hemanshu.mahajan@gmail.com>
2021-11-10 20:57:55 +0530

    Learning TDD

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, StyleSheet, View } from "react-native";
+import { Text } from "react-native-paper";
 
-export default function App() {
+const App = () => {
+  const [isToggled, setIsToggled] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body} testID="body">
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Hello World!</Text>
+      </View>
+      {isToggled && <Text>I am the toggled component</Text>}
+      <Button
+        title="Press me!"
+        onPress={() => setIsToggled(!isToggled)}
+        testID="pressMeButton"
+        accessibilityLabel="Press me"
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  body: {
+    backgroundColor: "#ffffff",
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "600",
+    color: "#000000",
   },
 });
+
+export default App;
